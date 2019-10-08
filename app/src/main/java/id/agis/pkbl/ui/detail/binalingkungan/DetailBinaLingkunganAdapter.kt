09c795d72029a518eb715e5789ec037f4255054c
agis <agis.septiyanto@gmail.com>
2019-10-08 14:11:48 +0700

    add ui improvement in list file

--- app/src/main/java/id/agis/pkbl/ui/detail/binalingkungan/DetailBinaLingkunganAdapter.kt
+++ app/src/main/java/id/agis/pkbl/ui/detail/binalingkungan/DetailBinaLingkunganAdapter.kt
@@ -1,15 +1,19 @@
 package id.agis.pkbl.ui.detail.binalingkungan
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import id.agis.pkbl.R
 import id.agis.pkbl.model.UserFile
-import kotlinx.android.synthetic.main.image_list.view.*
+import kotlinx.android.synthetic.main.file_list.view.*
+import org.jetbrains.anko.toast
+import java.io.File
 
 class DetailBinaLingkunganAdapter(
     val context: Context,
@@ -19,7 +23,7 @@ class DetailBinaLingkunganAdapter(
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_list, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.file_list, parent, false)
         return ViewHolder(view)
     }
 
@@ -28,20 +32,28 @@ class DetailBinaLingkunganAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.title.text = listFile[position].name
+        val file = listFile[position]
+        holder.title.text = file.name
         holder.itemView.setOnClickListener {
-            onItemClick(listFile[position])
+            onItemClick(file)
+            it.context.toast(file.uri.path!!)
         }
 
-//        if (listFile.isNotEmpty() && position <= listFile.size-1 && listFile[position] != null) {
-//            Glide.with(context).load(listFile[position]).into(holder.image)
-//        } else {
-//            Glide .with(context).load("https://via.placeholder.com/100").into(holder.image)
-//        }
+        if (file.type == "pdf") {
+            Glide.with(context).load(R.drawable.type_pdf).into(holder.icon)
+        } else if (file.type == "jpg" || file.type == "ong"){
+            val image = File(file.path)
+            val uri = Uri.fromFile(image)
+            Glide.with(context).load(uri).into(holder.image)
+            holder.image.visibility = View.VISIBLE
+        } else {
+            Glide.with(context).load(R.drawable.type_unknow).into(holder.icon)
+        }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val image: ImageView = itemView.image
+        val image: ImageView = itemView.iv_image
         val title: TextView = itemView.tv_title
+        val icon: ImageView = itemView.iv_icon
     }
 }
